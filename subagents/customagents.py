2aa1152d8f7c8f0e04117d21c97bb02a76077198
Madhu-712 <88141363+Madhu-712@users.noreply.github.com>
2025-11-14 16:26:16 +0530

    Create customagents.py

---
+++ subagents/customagents.py
@@ -0,0 +1,13 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+from google.adk.agents.llm_agent import Agent
+
+
+
+# Create an agent with google search tool as a search specialist
+google_search_agent = Agent(
+    model='gemini-2.5-flash',
+    name='googlewiki_search_agent',
+    instruction='Use google search or wiki to answer user questions about real-time, logistical information related to health care providers, hospitals and doctors info',
+    tools=[google_search],
+)
